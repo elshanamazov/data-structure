8395ca4a314dfda7f06eb875d104ecc8bb2d04c7
elshanamazov <elshanamazov@gmail.com>
2024-03-17 12:51:01 +0400

    feat: added push and pop methods

--- src/data-structures/stack/index.test.ts
+++ src/data-structures/stack/index.test.ts
@@ -12,4 +12,37 @@ describe('Stack', () => {
   test('should create Stack', () => {
     expect(stack).toBeDefined();
   });
+
+  describe('push', () => {
+    test('should push elements to the top of the stack', () => {
+      stack.push(1).push(2).push(3);
+
+      expect(stack.top?.data).toBe(3);
+      expect(stack.size).toBe(3);
+    });
+
+    test('should work correctly with push after pop operations', () => {
+      stack.push(1).push(2);
+      stack.pop();
+      stack.push(3);
+      expect(stack.top?.data).toBe(3);
+      expect(stack.size).toBe(2);
+    });
+  });
+
+  describe('pop', () => {
+    test('should return null from empty stack', () => {
+      expect(stack.pop()).toBeNull();
+    });
+
+    test('should remove element from the top of the stack', () => {
+      stack.push(1).push(2).push(3);
+      stack.pop();
+      expect(stack.top?.data).toBe(2);
+      expect(stack.size).toBe(2);
+      stack.pop();
+      expect(stack.top?.data).toBe(1);
+      expect(stack.size).toBe(1);
+    });
+  });
 });
