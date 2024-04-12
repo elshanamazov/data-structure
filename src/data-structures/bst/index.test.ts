70270e36023e8077a21f8dd94e72c3a88dd552c1
elshanamazov <elshanamazov@gmail.com>
2024-04-12 13:03:20 +0400

    feat(insert): added insert method for the BST

--- src/data-structures/bst/index.test.ts
+++ src/data-structures/bst/index.test.ts
@@ -13,4 +13,26 @@ describe('Binary Search Tree', () => {
     expect(binarySearchTree).toBeDefined();
     expect(binarySearchTree.root).toBeNull();
   });
+
+  describe('insert', () => {
+    test('should insert elements to the BST', () => {
+      binarySearchTree.insert(2)?.insert(1)?.insert(3);
+
+      expect(binarySearchTree.root?.data).toBe(2);
+      expect(binarySearchTree.root?.left?.data).toBe(1);
+      expect(binarySearchTree.root?.right?.data).toBe(3);
+    });
+
+    test('should set the first inserted element as the root', () => {
+      binarySearchTree.insert(10);
+      expect(binarySearchTree.root?.data).toBe(10);
+      expect(binarySearchTree.root?.left).toBeNull();
+      expect(binarySearchTree.root?.right).toBeNull();
+    });
+
+    test('should not insert equal data to the BST', () => {
+      binarySearchTree.insert(2)?.insert(1)?.insert(3);
+      expect(binarySearchTree.insert(3)).toBeUndefined();
+    });
+  });
 });
--- src/data-structures/bst/index.ts
+++ src/data-structures/bst/index.ts
@@ -2,4 +2,31 @@ import { BSTNode } from './bst-node';
 
 export class BST<T> {
   root: BSTNode<T> | null = null;
+
+  insert(data: T) {
+    const newNode = new BSTNode(data);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+
+    while (true) {
+      if (newNode.data === temp.data) return undefined;
+      if (newNode.data < temp.data) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
 }
