021823c0d21af4490853dc67428e80ddfb5cee64
elshanamazov <elshanamazov@gmail.com>
2024-05-14 11:37:27 +0400

    feat(set/get): add set and get methods for ht

--- src/data-structures/hash-tables/index.ts
+++ src/data-structures/hash-tables/index.ts
@@ -6,11 +6,34 @@ export class HashTable {
   }
 
   // eslint-disable-next-line no-underscore-dangle
-  private _hash(key: string): number {
+  private hash(key: string): number {
     let hash = 0;
     for (let i = 0; i < key.length; i += 1) {
-      hash += hash + ((key.charCodeAt(i) * 23) % this.dataMap.length);
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
     }
     return hash;
+  }
+
+  set(key: string, value: number) {
+    const index = this.hash(key);
+
+    if (!this.dataMap[index]) {
+      this.dataMap[index] = [];
+    }
+    this.dataMap[index].push([key, value]);
+    return this;
+  }
+
+  get(key: string) {
+    const index = this.hash(key);
+    if (this.dataMap[index]) {
+      for (let i = 0; i < this.dataMap[index].length; i += 1) {
+        if (this.dataMap[index][i][0] === key) {
+          return this.dataMap[index][i][1];
+        }
+      }
+    }
+
+    return undefined;
   }
 }
